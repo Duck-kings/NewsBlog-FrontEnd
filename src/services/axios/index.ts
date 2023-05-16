e9a096ddb9afca74b1a8e6fe222bb134515299ac
Duck-kings <sergiykovalenko6661@gmail.com>
2023-05-16 14:59:56 +0300

    Fix

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'http://localhost:5050/'
 });
 
 type axiosInstance = typeof instance;
